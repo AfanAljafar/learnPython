4734f04a07a29e21783d7c92e8326082ceb547d8
AfanAljafar <alzafarx@gmail.com>
2023-04-19 06:03:13 +0700

    python language

---
+++ README.md
@@ -0,0 +1,2 @@
+# LearnPython
+This space created for learn and train Python program!
---
+++ modul1.py
@@ -0,0 +1,8 @@
+print("enter your first number:")
+x = int (input())
+print("enter your second number:")
+y = int (input())
+
+z=x+y
+
+print("sum result first and second number is", z)
