9724a497008429b71f98ef427af9bfcf1c56834a
AfanAljafar <alzafarx@gmail.com>
2023-04-19 07:33:26 +0700

    python language

---
+++ modul 1/modul1a.py
@@ -0,0 +1,7 @@
+Name = str (input("Name\t: ")) 
+Class = str (input("Class\t: "))
+Study = str (input("Study\t: "))
+Faculty = str (input("Faculty\t: "))
+
+print("your bio")
+print(Name, " in class", Class, "studying", Study, "on Faculty", Faculty)
