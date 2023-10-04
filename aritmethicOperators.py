a9fdc37f4a031da65727d9e32c939cb3c98abe7f
afan <alzafarx@gmail.com>
2023-10-05 02:18:54 +0700

    exercises Python

--- aritmethicOperators.py
+++ aritmethicOperators.py
@@ -5,3 +5,52 @@ res = num1 + num2
 
 print(res)
 
+# addition operation
+print("=============================")
+print("addition operation")
+print("=============================")
+num1 = int(input("enter first number : "))
+num2 = int(input("enter second number : "))
+res = num1 + num2
+print("result : ", res)
+print("result : ", num1 + num2)
+
+# reduction operation
+print("=============================")
+print("reduction operation")
+print("=============================")
+num1 = int(input("enter first number : "))
+num2 = int(input("enter second number : "))
+res = num1 - num2
+print("result : ", res)
+print("result : ", num1 - num2)
+
+# multiplication operation
+print("=============================")
+print("multiplication operation")
+print("=============================")
+num1 = int(input("enter first number : "))
+num2 = int(input("enter second number : "))
+res = num1 * num2
+print("result : ", res)
+print("result : ", num1 * num2)
+
+# division operation
+print("=============================")
+print("division operation")
+print("=============================")
+num1 = int(input("enter first number : "))
+num2 = int(input("enter second number : "))
+res = num1 / num2
+print("result : ", res)
+print("result : ", num1 / num2)
+
+# modulus operation
+print("=============================")
+print("modulus operation")
+print("=============================")
+num1 = int(input("enter first number : "))
+num2 = int(input("enter second number : "))
+res = num1 % num2
+print("result : ", res)
+print("result : ", num1 % num2)
